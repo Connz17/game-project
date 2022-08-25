1cdef084d003d1507ca1d2bdac5db4e056c36d08
Connel Nwaodor <connel_n@hotmail.com>
2022-08-25 10:45:53 +0100

    Buttons assigned in JS

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("im in");
+// collate all game squares
+pathways = document.getElementsByClassName("game__path");
+console.log(pathways);
+thorns = document.getElementsByClassName("game__thorns");
+console.log(thorns);
+bombs = document.getElementsByClassName("game__bomb");
+console.log(bombs);
+gateways = document.getElementsByClassName("game__gate");
+console.log(gateways); // get start button
+
+startButton = document.getElementById("game-start");
+console.log(startButton); // get movement buttons
+
+characterMovement = document.getElementsByClassName("movement__buttons");
+console.log(characterMovement);
